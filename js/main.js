cb0213675173d8460652eda92b7f5deed4a489a3
Robiul-Hasan-developer <83179892+Robiul-Hasan-developer@users.noreply.github.com>
2021-04-26 18:14:19 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// bx slider========================================//
+$(document).ready(function(){
+    $('.bx_slider').bxSlider();
+    $('#navigation_area').meanmenu({
+        meanMenuContainer: '#mobile_menu',
+        meanScreenWidth: 991,
+    });
+});
